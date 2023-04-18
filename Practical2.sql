026b3b532ca4e8f555b89719a8b1253964f11fb7
jimitpatel3699 <jimitpatel3699@users.noreply.github.com>
2023-04-18 15:21:42 +0530

    case statment scenaroi added and correct syntex

--- Practical2.sql
+++ Practical2.sql
@@ -1,19 +1,22 @@
---Create table department.
-create table Department(Id int primary key,DepartmentName varchar(200));
+--Create table Department.
+CREATE TABLE Department(Id INT PRIMARY KEY,DepartmentName VARCHAR(200) NOT NULL);
 --Create table Employee.
-create table Employee(Id int primary key identity(1,1),
-EmployeeName varchar(20) not null,Department_Id int references Department(Id),Experience decimal(3,1),
-Salary money);
---Insert into department.
-insert into Department values
+CREATE TABLE Employee(Id INT PRIMARY KEY IDENTITY(1,1),
+EmployeeName VARCHAR(20) NOT NULL,Department_Id INT REFERENCES Department(Id) NOT NULL,
+Experience DECIMAL(3,1) CHECK(Experience>0),Salary MONEY);
+--Alter table Employee.
+ALTER TABLE Employee ADD CONSTRAINT CHK_Employee_Salary CHECK(Salary>0);
+--Insert into Department.
+INSERT INTO Department VALUES
 (1,'Accounting'),
 (2,'Research'),
 (3,'Sales'),
 (4,'Operations'),
 (5,'Marketing'),
-(6,'IT');
+(6,'IT'),
+(7,'Admin');
 --Insert into Employee.
-insert into Employee values 
+INSERT INTO Employee VALUES 
 ('John Smith', 3, 3.0, 50000.00),
 ('Jane Doe', 5, 1.5, 40000.00),
 ('Bob Johnson', 3, 9.0, 55000.00),
@@ -35,33 +38,38 @@ insert into Employee values
 ('Ashley Brown', 4, 6.0, 49000.00),
 ('Oliver Wilson', 6, 5.0, 38000.00);
 --Write a query display empid,empname and departmentname.
-select e.id,e.EmployeeName,d.DepartmentName from Department d
-inner join Employee e
-on d.Id = e.Department_Id;
+SELECT e.id,e.EmployeeName,d.DepartmentName FROM Department d
+LEFT JOIN Employee e
+ON d.Id = e.Department_Id;
 --Write a query to display department wise employee count.
-select d.DepartmentName,count(e.Department_Id) EmployeeCount from Department d
-inner join Employee e
-on d.Id = e.Department_Id 
-group by d.DepartmentName;
+SELECT d.DepartmentName,COUNT(e.Department_Id) EmployeeCount FROM Department d
+LEFT JOIN Employee e
+ON d.Id = e.Department_Id 
+GROUP BY d.DepartmentName;
 --Write a query to display department wise maximum salary.
-select d.DepartmentName,max(e.Salary) EmployeeCount from Department d
-inner join Employee e
-on d.Id = e.Department_Id 
-group by d.DepartmentName;
+SELECT d.DepartmentName,MAX(e.Salary) MaximumSalary FROM Department d
+INNER JOIN Employee e
+ON d.Id = e.Department_Id 
+GROUP BY d.DepartmentName ORDER BY MaximumSalary DESC;
 --Write a query to display employee name in the ascending order of department name.
-select e.EmployeeName from Department d
-inner join Employee e
-on d.Id = e.Department_Id 
-order by d.DepartmentName;
+SELECT e.EmployeeName,d.DepartmentName FROM Department d
+INNER JOIN Employee e
+ON d.Id = e.Department_Id 
+ORDER BY d.DepartmentName;
 /*Write a query to display employeeName and grade where grade criteria is as below
 if employee has more than 10 years of experience then grade is expert
 if employee has experience between 7 to 10 years then its grade is advanced
 if employee has experience between 5 to 7 years then its grade is intermediate
 if employee has experience between 2 to 5 year then its grade is beginner
 and for the rest of case display grade as novice.*/
+SELECT EmployeeName,Experience,
+	CASE 
+	WHEN Experience > 10 THEN 'expert'
+	WHEN Experience BETWEEN 7 AND 10 THEN 'advanced'
+	WHEN Experience BETWEEN 5 AND 7 THEN 'intermediate'
+	WHEN Experience BETWEEN 2 AND 5 THEN 'beginner'
+	ELSE 'novice'
+	END AS grade
+FROM Employee;
 
-select * from Department d
-inner join Employee e
-on d.Id = e.Department_Id order by d.DepartmentName,e.Salary;
-select * from Department,Employee;
-select * from Employee order by Experience;
+
